0c2e21441448bf2850bf154a06b0032c016c734d
Maarten <maartenwachtersmast@gmail.com>
2017-05-05 20:34:51 +0200

    Added client and server

---
+++ server.js
@@ -0,0 +1,43 @@
+//Module requirements
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var bodyparser = require('body-parser');
+
+var app = express();
+app.use(express.static('client'));
+
+var db; // Initialiseren van mongoDB variabele naam
+var dataTable; //Opslagen van data in formaat
+			   //"timestamp": ISODate(), "naam": "", "pulse": pulse
+			   //"timestamp" : ISODate(), "Naam": "Maarten"; "pulse": 67
+
+						//TODO Database link here
+	MongoClient.connect('mongodb://localhost:27017/testmaarten', function (err, _db){
+		if (err) throw err;
+		if (err) console.log(err);
+		console.log("Connected to database");
+		db = _db //Als error is , weergeef error
+		dataTable = db.collection('data');
+
+	});
+
+	//MOGELIJKE BEMERKINGEN
+	//Data weergeven. Naam in textbox? > toon vakje met naam , bpm en laatste tijd
+						//Hoe constant automatisch refreshen?
+
+	app.use(bodyparser.json());
+
+	//Break database connection
+	process.on('SIGINT', function(){
+		console.log("Server wordt afgesloten");
+		db.close();
+		process.exit(0);
+	});
+
+	app.use(function(req, res, next) {
+	  	res.header("Access-Control-Allow-Origin", "*");
+	  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  	next();
+	});
+
+	app.listen(3000);
